7424552d4b475364c60593952e016ca555765f1d
rwntlpt <67883450+rwntlpt@users.noreply.github.com>
2026-05-03 16:04:11 +0000

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_rwnt_vgatest user_project (
+   tt_um_rwnt_vgatest(
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
